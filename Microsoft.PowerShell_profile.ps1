e571793689b7d6d9fe035a1c75d4a26272754ef0
Rafe Hart <rhart@starrez.com>
2017-03-21 16:16:31 +1100

    Initial Powershell Script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,57 @@
+# %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+[console]::backgroundcolor = [ConsoleColor]::Black
+set-location C:\Users\rhart\Dropbox\Computers\Projects
+
+
+
+# Make powershell more like bash
+function env {
+	Get-ChildItem Env:"$args"
+}
+
+function touch {
+  New-Item "$args" -ItemType File
+}
+
+new-alias grep Select-String
+
+
+
+
+# To Test
+function adminprocess {
+	Start-Process cmd -Verb RunAs
+}
+
+function version {
+	$PSVersionTable.PSVersion
+}
+
+
+
+function reloadpath {
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+set-alias notepad "C:\Program Files\Notepad++\notepad++.exe"
+set-alias zip "C:\Program Files\7-Zip\7z.exe"
+
+
+# quick ways to navigate around the system, e.g. cd $documents
+$tools = "c:\tools"
+$winsitter = "c:\code\winsitter"
+$vstools = "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools"
+$documents = $home + "\Documents"
+$desktop = $home + "\Desktop"
+$downloads = $home + "\Downloads"
+$modules = $home + "\Documents\WindowsPowerShell\Modules"
+
+
