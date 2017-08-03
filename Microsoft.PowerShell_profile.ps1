a8e3ebbc9d2489ef3d6e50a1f4e90ba1f71a0ff9
Rafael Hart <rafael.hart9@gmail.com>
2017-08-03 17:03:51 +1000

    Added Project alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
 [console]::backgroundcolor = [ConsoleColor]::Black
-set-location C:\Users\rhart\Dropbox\Computers\Projects
+set-location C:\Users\$env:USERNAME\Dropbox\Computers\Projects
 
 
 
@@ -20,8 +20,15 @@ function apache {
 	set-location "C:\Users\$env:USERNAME\AppData\Roaming\Apache24"
 }
 
+function projects {
+	set-location "C:\Users\$env:USERNAME\Dropbox\Computers\Projects"
+}
 function Documents {
 	set-location "C:\Users\$env:USERNAME\Documents"
+}
+
+function version {
+	$PSVersionTable.PSVersion
 }
 
 # To Test
@@ -29,9 +36,7 @@ function adminprocess {
 	Start-Process cmd -Verb RunAs
 }
 
-function version {
-	$PSVersionTable.PSVersion
-}
+
 
 
 function reloadpath {
@@ -48,14 +53,5 @@ if (Test-Path($ChocolateyProfile)) {
 set-alias notepad "C:\Program Files\Notepad++\notepad++.exe"
 set-alias zip "C:\Program Files\7-Zip\7z.exe"
 
-
-# quick ways to navigate around the system, e.g. cd $documents
-$tools = "c:\tools"
-$winsitter = "c:\code\winsitter"
-$vstools = "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools"
-$documents = $home + "\Documents"
-$desktop = $home + "\Desktop"
-$downloads = $home + "\Downloads"
-$modules = $home + "\Documents\WindowsPowerShell\Modules"
 
 
