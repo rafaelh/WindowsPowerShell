40a14fc7454426e46caa6018a7e246c5494c5cb3
Rafe Hart <rafael.hart9@gmail.com>
2019-04-16 08:17:52 +1000

    Adding linking function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,15 @@
 # %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
+
 # Shell Variables
 [console]::backgroundcolor = [ConsoleColor]::Black
 $BETTER_EXCEPTIONS = 1
 
+
 # Make powershell more like bash
 function env {Get-ChildItem Env:"$args"}
 function touch {New-Item "$args" -ItemType File}
+function ln ($target, $link) {New-Item -Path $link -ItemType SymbolicLink -Value $target}
 new-alias grep Select-String
 
 
